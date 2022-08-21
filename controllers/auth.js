5805a51783d7d164c2893ea051f4c46799e9d6b5
Carlos Zabala <czabala847@gmail.com>
2022-08-20 20:10:31 -0500

    Validando si el correo ya existe en BD

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,15 +2,25 @@ const { response } = require("express");
 const User = require("../models/User");
 
 const createUser = async (req, res = response) => {
-  // const { name, email, password } = req.body;
+  const { email } = req.body;
 
   try {
-    const user = new User(req.body);
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Un usuario existe con ese correo.",
+      });
+    }
+
+    user = new User(req.body);
     await user.save();
 
     res.status(201).json({
       ok: true,
-      msg: "registro",
+      uid: user.id,
+      name: user.name,
     });
   } catch (error) {
     console.log(error);
