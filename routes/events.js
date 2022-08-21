44bdcbc064615b70732f06f5ae87411c30b61f5f
Carlos Zabala <czabala847@gmail.com>
2022-08-20 21:38:58 -0500

    Validando campos obligatorios para crear eventos

--- routes/events.js
+++ routes/events.js
@@ -4,6 +4,7 @@
  */
 
 const { Router } = require("express");
+const { check } = require("express-validator");
 const router = Router();
 
 const {
@@ -12,13 +13,24 @@ const {
   updateEvent,
   deleteEvent,
 } = require("../controllers/events");
+const { validateFields } = require("../middlewares/validateFields");
 const { validateJWT } = require("../middlewares/validateJWT");
+const { isDate } = require("../helpers/isDate");
 
 //Todas las rutas tienen que pasar por la validación del JWT
 router.use(validateJWT);
 
 router.get("/", getEvents);
-router.post("/", createEvent);
+router.post(
+  "/",
+  [
+    check("title", "El titulo es obligatorio").not().isEmpty(),
+    check("start", "Fecha de inicio es obligatoria").custom(isDate),
+    check("end", "Fecha de finalización es obligatoria").custom(isDate),
+    validateFields,
+  ],
+  createEvent
+);
 router.put("/:id", updateEvent);
 router.delete("/:id", deleteEvent);
 
