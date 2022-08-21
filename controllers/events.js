9b2a23954d243f4919d3292f4c407ee8bcc1b9b1
Carlos Zabala <czabala847@gmail.com>
2022-08-20 21:50:48 -0500

    Guardando eventos en BD

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const Event = require("../models/Event");
 
 const getEvents = async (req, res = response) => {
   res.json({
@@ -8,10 +9,24 @@ const getEvents = async (req, res = response) => {
 };
 
 const createEvent = async (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: "createEvent",
-  });
+  const event = new Event(req.body);
+
+  try {
+    event.user = req.uid;
+    const eventSaved = await event.save();
+
+    res.json({
+      ok: true,
+      event: eventSaved,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
 };
 
 const updateEvent = async (req, res = response) => {
